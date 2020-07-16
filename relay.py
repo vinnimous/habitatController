fd08b7ba01b625855f2a33bd5b0d960205236211
Vinnimous <vinnimous@users.noreply.github.com>
2020-07-16 16:50:14 -0400

    Develop (#7)
    * removed time because it is already there
    
    * cleaned up some imports
    
    * don't know why I had relay in there, it isn't needed
    
    * merging back to master I guess
    
    * maybe I fixed it
    
    * maybe I fixed it
    
    * wrong number
    
    * removed other conditional
    
    * refactored and messed up
    
    * INVERTED THE state

--- relay.py
+++ relay.py
@@ -11,32 +11,32 @@ pin_night = 16
 def setup():
     GPIO.setwarnings(False)    # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-    GPIO.setup(pin_light, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_uvb, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_heater, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_night, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(pin_light, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(pin_uvb, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(pin_heater, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(pin_night, GPIO.OUT, initial=GPIO.HIGH)
 
 def day_light():
-    GPIO.output(pin_light, GPIO.HIGH) # Turn on light bulb
-    GPIO.output(pin_uvb, GPIO.HIGH) # Turn on UVB light
-    GPIO.output(pin_night, GPIO.LOW)  # Turn off night light
+    GPIO.output(pin_light, GPIO.LOW) # Turn on light bulb
+    GPIO.output(pin_uvb, GPIO.LOW) # Turn on UVB light
+    GPIO.output(pin_night, GPIO.HIGH)  # Turn off night light
 def night_light():
-    GPIO.output(pin_light, GPIO.LOW)  # Turn off light bulb
-    GPIO.output(pin_uvb, GPIO.LOW)  # Turn off UVB light
-    GPIO.output(pin_night, GPIO.HIGH) # Turn on night light
+    GPIO.output(pin_light, GPIO.HIGH)  # Turn off light bulb
+    GPIO.output(pin_uvb, GPIO.HIGH)  # Turn off UVB light
+    GPIO.output(pin_night, GPIO.LOW) # Turn on night light
 def emergency_heat():
-    GPIO.output(pin_light, GPIO.HIGH)  # Turn on light bulb
-    GPIO.output(pin_uvb, GPIO.HIGH)  # Turn on UVB light
-    GPIO.output(pin_heater, GPIO.HIGH)  # Turn on heat
-    GPIO.output(pin_night, GPIO.HIGH)  # Turn on night light
+    GPIO.output(pin_light, GPIO.LOW)  # Turn on light bulb
+    GPIO.output(pin_uvb, GPIO.LOW)  # Turn on UVB light
+    GPIO.output(pin_heater, GPIO.LOW)  # Turn on heat
+    GPIO.output(pin_night, GPIO.LOW)  # Turn on night light
 def all_off():
-    GPIO.output(pin_light, GPIO.LOW)  # Turn off light bulb
-    GPIO.output(pin_uvb, GPIO.LOW)  # Turn off UVB light
-    GPIO.output(pin_heater, GPIO.LOW)  # Turn off heat
-    GPIO.output(pin_night, GPIO.LOW)  # Turn off night light
+    GPIO.output(pin_light, GPIO.HIGH)  # Turn off light bulb
+    GPIO.output(pin_uvb, GPIO.HIGH)  # Turn off UVB light
+    GPIO.output(pin_heater, GPIO.HIGH)  # Turn off heat
+    GPIO.output(pin_night, GPIO.HIGH)  # Turn off night light
 
 def heater_on():
-    GPIO.output(pin_heater, GPIO.HIGH)
+    GPIO.output(pin_heater, GPIO.LOW)
 
 def heater_off():
-    GPIO.output(pin_heater, GPIO.LOW)
+    GPIO.output(pin_heater, GPIO.HIGH)
