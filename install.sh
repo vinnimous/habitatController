7f96c1575be290c5c96f2d375a0595a0d2dda1fb
vinny <ron.valenzuela81@gmail.com>
2020-12-04 14:56:26 -0500

    I think I need to fix the shell script

--- install.sh
+++ install.sh
@@ -3,10 +3,17 @@
 sudo apt-get install -y build-essential python-dev python-smbus python3-pip python-mysqldb
 #https://grafana.com/docs/grafana/latest/installation/debian/
 sudo apt-get install -y apt-transport-https software-properties-common wget mariadb-server
+
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana-rpi_7.3.4_armhf.deb
+sudo dpkg -i grafana-rpi_7.3.4_armhf.deb
+
+
+
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get install -y grafana
 sudo service grafana-server start
 sudo service grafana-server status
 sudo update-rc.d grafana-server defaults
@@ -14,9 +21,6 @@ sudo update-rc.d grafana-server defaults
 sudo mysql_secure_installation
 sudo mysql -u root -p
 
-sudo mysql_secure_installation
-
-sudo mysql -u root -p
 
 CREATE DATABASE IF NOT EXISTS habitatHistoryDB;
 
