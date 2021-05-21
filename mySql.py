1d125464d5868d8611dcb0198587d2f9206499a6
vinny <ron.valenzuela81@gmail.com>
2021-05-21 16:55:34 -0400

    upldated sql statement

--- mySql.py
+++ mySql.py
@@ -43,10 +43,11 @@ def insert(date, tod, season, temp_set, temp_act, light_uvb, light_day, light_ni
 def delete_old():
     db = pymysql.connect(sqlHost, sqlUser, sqlPass, sqlDB)
     cursor = db.cursor()
-    sql = "DELETE FROM from habitatHistoryTable where DATE < now() - interval %s DAY"
-    adr = sqlArchiveLimit
+    p1 = "DELETE FROM from habitatHistoryTable where DATE < now() - interval "
+    p2 = " DAY;"
+    sql = p1 + str(sqlArchiveLimit) + p2
     try:
-        cursor.execute(sql, (adr,))
+        cursor.execute(sql)
         db.commit()
     except Exception as e:
         logger.error(e)
