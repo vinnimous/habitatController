62c09eb0dfdbc01af83e4772cb0ad9d09af67a3a
vinny <ron.valenzuela81@gmail.com>
2021-10-19 12:48:10 -0400

    Corrected duplication in the files and cleaned it up a bit

--- test.py
+++ test.py
@@ -7,9 +7,12 @@ next_event = mapSun.sunrise
 current_event = mapSun.sunset
 diff = current_event-next_event
 sofar = 100-((next_event - datetime.datetime.now())/diff)*100
+lightLog = "\"Incandescent: {} UVB: {} Heat: {} Night: {}\"" \
+           ".format(GPIO.input(pin_light), (GPIO.input(pin_uvb)), GPIO.input(pin_heater), GPIO.input(pin_night))"
 print("Dawn: %s, Sunrise: %s, Noon: %s, Sunset: %s, Dusk: %s" % (mapSun.dawn, mapSun.sunrise, mapSun.noon, mapSun.sunset, mapSun.dusk))
 
 print("Now:\t\t %s \nNoon:\t\t %s \nSunrise:\t %s" %(datetime.datetime.now(), mapSun.noon, mapSun.sunrise))
 print("Difference:\t %s" %(diff))
 
 print("So far: \t%s" %(sofar))
+print(lightLog)
