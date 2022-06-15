2bb4ce1be671498b34025e3b636a9358ecfb4603
vinny <ron.valenzuela81@gmail.com>
2022-06-15 12:19:57 -0400

    updated sun mapping due to update

--- mapSun.py
+++ mapSun.py
@@ -25,7 +25,12 @@ def new_day():
 
 
 def current_times():
-    geoloc = LocationInfo(' '.join(map(str, read_configs())))
+    geoloc = LocationInfo()
+    geoloc.name = read_configs().name
+    geoloc.region = read_configs().region
+    geoloc.timezone = read_configs().timezone
+    geoloc.latitude = read_configs().latitude
+    geoloc.longitude = read_configs().longitude
     s = sun(geoloc.observer, datetime.datetime.now(), tzinfo=geoloc.timezone)
     global dawn, sunrise, noon, sunset, dusk
     dawn = s["dawn"].replace(tzinfo=None)
@@ -37,15 +42,23 @@ def current_times():
 
 
 def read_configs():
-    configs_loc = []
     parser = ConfigParser()
     parser.read(path.join(path.dirname(path.abspath(__file__)), configs))
     if parser.has_section(section):
-        configs_loc.append(parser.get(section, "city"))
-        configs_loc.append(parser.get(section, "state"))
-        configs_loc.append(parser.get(section, "timezone"))
-        configs_loc.append(parser.get(section, "latitude"))
-        configs_loc.append(parser.get(section, "longitude"))
+        name = parser.get(section, "name")
+        region = parser.get(section, "region")
+        timezone = parser.get(section, "timezone")
+        latitude = parser.get(section, "latitude")
+        longitude = parser.get(section, "longitude")
     else:
         raise Exception('{0} not found in the {1} file'.format(section, configs))
-    return configs_loc
+    return Loc(name, region, timezone, latitude, longitude)
+
+
+class Loc:
+    def __init__(self, name, region, timezone, latitude, longitude):
+        self.name = name
+        self.region = region
+        self.timezone = timezone
+        self.latitude = latitude
+        self.longitude = longitude
