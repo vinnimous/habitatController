27047080b73513175baacfaf7a3c5c87b61198b1
vinnimous <ron.valenzuela81@gmail.com>
2025-02-17 19:21:54 -0500

    Changes to be committed: 	modified:   install.sh 	modified:   uninstall.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Define colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 RASP_MOD_A="Raspberry Pi Model A"
 RASP_MOD_B="Raspberry Pi 3 Model B"
 MODEL_FILE="/sys/firmware/devicetree/base/model"
@@ -17,56 +22,56 @@ IS_GRAFANA_RUNNING=false
 
 # Determine Raspberry Pi architecture to select correct packages
 get_arch() {
-  echo "Detecting Raspberry Pi type"
+  echo -e "${GREEN}Detecting Raspberry Pi type${NC}"
   if grep -q "$RASP_MOD_A" "$MODEL_FILE"; then
-    echo "$RASP_MOD_A detected"
+    echo -e "${GREEN}$RASP_MOD_A detected${NC}"
     IS_TYPE_A=true
   elif grep -q "$RASP_MOD_B" "$MODEL_FILE"; then
-    echo "$RASP_MOD_B detected"
+    echo -e "${GREEN}$RASP_MOD_B detected${NC}"
     IS_TYPE_B=true
   else
-    echo "Undetected Raspberry Pi Model"
+    echo -e "${RED}Undetected Raspberry Pi Model${NC}"
   fi
 }
 
 # Update package lists
 updating() {
-  echo "Updating libraries"
+  echo -e "${GREEN}Updating libraries${NC}"
   sudo apt-get update -y
 }
 
 # Install basic build tools and Python dependencies
 install_basics() {
-  echo "Installing some basic build tools"
+  echo -e "${GREEN}Installing some basic build tools${NC}"
   sudo apt-get install -y build-essential python3-dev python3-smbus python3-pip python3-venv
 }
 
 # Set up Python virtual environment and install requirements
 install_requirements() {
-  echo "Setting up Python virtual environment"
+  echo -e "${GREEN}Setting up Python virtual environment${NC}"
   python3 -m venv venv  # Create a virtual environment named 'venv'
   source venv/bin/activate  # Activate the virtual environment
-  echo "Installing requirements in the virtual environment"
+  echo -e "${GREEN}Installing requirements in the virtual environment${NC}"
   pip install -r requirements.txt  # Install packages in the virtual environment
   deactivate  # Deactivate the virtual environment
 }
 
 # Install MySQL and related packages
 install_mysql() {
-  echo "Installing items for MySQL"
+  echo -e "${GREEN}Installing items for MySQL${NC}"
   sudo apt-get install -y apt-transport-https software-properties-common wget mariadb-server adduser libfontconfig1
   sudo apt-get install -y python3-mysqldb expect
 }
 
 # Select which Grafana package to install based on Raspberry Pi architecture
 install_grafana() {
-  echo "Installing Grafana"
+  echo -e "${GREEN}Installing Grafana${NC}"
   if $IS_TYPE_B; then
     grafana_apt
   elif $IS_TYPE_A; then
     grafana_deb
   else
-    echo "$ERROR_MOD_NOT_FOUND"
+    echo -e "${RED}$ERROR_MOD_NOT_FOUND${NC}"
   fi
 }
 
@@ -86,7 +91,7 @@ grafana_apt() {
 
 # Start Grafana service
 start_grafana() {
-  echo "Starting Grafana"
+  echo -e "${GREEN}Starting Grafana${NC}"
   if $IS_TYPE_B; then
     sudo /bin/systemctl enable grafana-server
     sudo /bin/systemctl start grafana-server &
@@ -94,23 +99,23 @@ start_grafana() {
     sudo service grafana-server start &
     sudo update-rc.d grafana-server defaults
   else
-    echo "$ERROR_MOD_NOT_FOUND"
+    echo -e "${RED}$ERROR_MOD_NOT_FOUND${NC}"
   fi
 }
 
 # Check if Grafana is running
 check_grafana() {
   if systemctl is-active grafana-server | grep -q "$GRAFANA_RUNNING"; then
-    echo "Grafana is currently running"
+    echo -e "${GREEN}Grafana is currently running${NC}"
     IS_GRAFANA_RUNNING=true
   else
-    echo "Grafana may have failed, please confirm manually"
+    echo -e "${RED}Grafana may have failed, please confirm manually${NC}"
   fi
 }
 
 # Set up MySQL with secure installation
 setup_mysql() {
-  echo "Setting up MySQL"
+  echo -e "${GREEN}Setting up MySQL${NC}"
   expect <<EOF
 spawn sudo mysql_secure_installation
 
@@ -140,15 +145,15 @@ EOF
 
 # Create a cron job to run the main script at reboot
 create_cron() {
-  echo "Trying to create a cronjob"
+  echo -e "${GREEN}Trying to create a cronjob${NC}"
   (
     crontab -l
     echo "$CRONJOB"
   ) | awk '!x[$0]++' | crontab -
   if crontab -l | grep -q "$CRONJOB"; then
-    echo "Successfully created cronjob"
+    echo -e "${GREEN}Successfully created cronjob${NC}"
   else
-    echo "Cronjob creation failed, attempting schedule"
+    echo -e "${RED}Cronjob creation failed, attempting schedule${NC}"
     create_schedule
   fi
 }
@@ -167,7 +172,7 @@ configure_grafana() {
 
 # Clean up and restart the system
 restart() {
-  echo "Removing leftovers and restarting"
+  echo -e "${GREEN}Removing leftovers and restarting${NC}"
   sudo apt autoremove -y
   sudo shutdown -r
 }
--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Define colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 RASP_MOD_A="Raspberry Pi Model A"
 RASP_MOD_B="Raspberry Pi 3 Model B"
 MODEL_FILE="/sys/firmware/devicetree/base/model"
 IS_TYPE_A=false
 IS_TYPE_B=false
 ERROR_MOD_NOT_FOUND="Undetected Raspberry Pi Model"
-CRONJOB="@reboot python3 /home/pi/habitatController/main.py"
+CRONJOB="@reboot /home/pi/habitatController/venv/bin/python3 /home/pi/habitatController/main.py"
 CRONFILE="/var/spool/cron/crontabs/pi"
 CRONCAT="sudo cat $CRONFILE"
 TMPFILE="/tmp/tmpCronJobs.txt"
@@ -17,35 +22,56 @@ GRAFANA_RUNNING="Active: active (running)"
 IS_GRAFANA_ENABLED=false
 IS_GRAFANA_RUNNING=false
 
+# Determine Raspberry Pi architecture
 get_arch() {
-  echo "Detecting Raspberry Pi type"
+  echo -e "${GREEN}Detecting Raspberry Pi type${NC}"
   if grep -q "$RASP_MOD_A" "$MODEL_FILE"; then
-    echo "$RASP_MOD_A detected"
+    echo -e "${GREEN}$RASP_MOD_A detected${NC}"
     IS_TYPE_A=true
   elif grep -q "$RASP_MOD_B" "$MODEL_FILE"; then
-    echo "$RASP_MOD_B detected"
+    echo -e "${GREEN}$RASP_MOD_B detected${NC}"
     IS_TYPE_B=true
   else
-    echo "Undetected Raspberry Pi Model"
+    echo -e "${RED}Undetected Raspberry Pi Model${NC}"
   fi
 }
 
+# Uninstall MySQL and related packages
 uninstall_mysql() {
-  echo "Installing items for mySql"
-  sudo apt-get remove -y mariadb-server python-mysqldb
+  echo -e "${GREEN}Uninstalling MySQL${NC}"
+  sudo apt-get remove -y mariadb-server python3-mysqldb
 }
 
+# Uninstall Grafana
 uninstall_grafana() {
+  echo -e "${GREEN}Uninstalling Grafana${NC}"
   sudo apt-get remove -y grafana
   sudo rm -f /etc/apt/sources.list.d/grafana.list
   sudo userdel -r grafanauser
 }
 
+# Remove Python virtual environment and related packages
+uninstall_python_env() {
+  echo -e "${GREEN}Removing Python virtual environment${NC}"
+  rm -rf /home/pi/habitatController/venv
+}
+
+# Remove cron job
+remove_cron() {
+  echo -e "${GREEN}Removing cron job${NC}"
+  crontab -l | grep -v "$CRONJOB" | crontab -
+}
+
+# Clean up system
 uninstall_cleanup() {
+  echo -e "${GREEN}Cleaning up system${NC}"
   sudo apt autoremove -y
 }
 
+# Execute the functions in order
 get_arch
 uninstall_mysql
 uninstall_grafana
+uninstall_python_env
+remove_cron
 uninstall_cleanup
